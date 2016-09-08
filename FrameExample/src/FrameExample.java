8130813162fd4bcf88fbb0ad34abeae112fcbaca
andreaszdw <andreaszdw@googlemail.com>
2016-09-08 11:17:40 +0200

    Nur zum testen!

---
+++ FrameExample/src/FrameExample.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class FrameExample {
+
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("Sample JFrame");
+		myFrame.setSize(200, 200);
+		myFrame.setVisible(true);
+
+	}
+
+}
